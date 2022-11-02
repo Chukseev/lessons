d1e5fae21d303ba446e4c34556991b52f3f9e34c
Chukseev <chukseev.nikita@yandex.ru>
2022-11-02 20:02:26 +0300

    Revert "lesson-5"
    This reverts commit a15fb21d89787eacaba14e531f20d1f3bf54ba91.

--- ex_5.py
+++
@@ -1,9 +0,0 @@
-with open('file_for_ex_5.txt', 'w', encoding='utf-8') as file:
-    my_numbers = input('Введите числа разделенные пробелом --> ')
-    file.write(my_numbers)
-with open('file_for_ex_5.txt', encoding='utf-8') as file:
-    result = file.read().split(' ')
-    new_result = 0
-    for el in result:
-        new_result += int(el)
-    print(new_result)
